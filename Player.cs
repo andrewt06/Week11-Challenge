2b04018ba26224f2f56d50ddac0f51231fbac0d4
andrewt06 <drewnolan6@gmail.com>
2025-11-02 15:15:10 -0500

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,9 +13,10 @@ public class Player : MonoBehaviour
 
     private float playerSpeed;
     private float horizontalInput;
- 
+    private float verticalInput;
 
     private float horizontalScreenLimit = 9.5f;
+    private float verticalScreemlimit = -3f;
     
 
     public GameObject bulletPrefab;
@@ -23,7 +25,8 @@ public class Player : MonoBehaviour
     {
         playerSpeed = 6f;
         //This function is called at the start of the game
-        
+        horizontalInput = -1.0f;
+        verticalInput = -3.0f;
     }
 
     void Update()
@@ -37,7 +40,7 @@ public class Player : MonoBehaviour
     void Shooting()
     {
         //if the player presses the SPACE key, create a projectile
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(bulletPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
@@ -47,15 +50,23 @@ public class Player : MonoBehaviour
     {
         //Read the input from the player
         horizontalInput = Input.GetAxis("Horizontal");
-        
+        verticalInput = Input.GetAxis("Vertical");
         //Move the player
-        transform.Translate(new Vector3(horizontalInput, 0) * Time.deltaTime * playerSpeed);
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
         //Player leaves the screen horizontally
-        if(transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
+   
+            {
+                transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+            }
+        //Player leaves the screen vertically
+        if (transform.position.y > 0 || transform.position.y <= -3)
+      
+               
+           
         {
-            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y * 0, 0);
         }
-        
-    }
 
+    }
 }
